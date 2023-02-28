91e558615be185426a4d28931549926b50c180ed
tima.kolesnik.2015@mail.ru <tima.kolesnik.2015@mail.ru>
2023-02-28 15:16:23 +0200

    Добавьте файлы проекта.

---
+++ Selenium/Program.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+var list = new List<string>()
+{
+    "На базе 9 классов",
+    "На базе 11 классов",
+    "Бюджет",
+    "Платное",
+    "Очно",
+    "Очно-заочно",
+    "Заочно",
+    "Дистанционно"
+};
+
+IWebDriver driver = new ChromeDriver();
+
+driver.Url = @"https://postupi.info/college/5/spec/448";
+
+driver.FindElement(By.CssSelector(".checkbox"));
+
+//while (true)
+//{
+//    Task.Delay(1000).Wait();
+
+//    driver.FindElement(By.XPath("//label[@for='budget'][contains(.,'Бюджет')]")).Click();
+//}
+
+while (true)
+{
+    list.ForEach(x =>
+    {
+        Task.Delay(1000).Wait();
+
+        driver.FindElement(By.XPath($"//label[contains(.,'{x}')]")).Click();
+    });
+}
+
+
+Console.Read();
